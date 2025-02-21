3c2659e75f7e3e388bec22a87e0fe27f7572b15e
edoscoba <obisimon86@gmail.com>
2025-02-21 19:52:13 +0100

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = () =>{
 
 	let text = document.getElementsByClassName("wish")[0];
 	
-	aakuWriter(text, "On this special day, I just want to remind you how incredible you are. You are an amazing girl that every man wishes to have in their life beautiful inside and out, kind-hearted, and truly one of a kind. Your smile lights up every room, and your presence makes everything feel better. I hope today brings you as much happiness, love, and joy as you bring to everyone around you. May this new year of your life be filled with endless blessings, unforgettable moments, and all your heart’s desires. Enjoy your day, Queen! You deserve the world and more.✨❤️");
+	aakuWriter(text, "You are an amazing girl that every man wishes to have in their life—beautiful, kind, and truly one of a kind. Your smile lights up every room, and your presence is a blessing. May your day be filled with love, joy, and all your heart desires.Enjoy your special day, Queen! You deserve the world and more.✨❤️");
 	
 };
 
